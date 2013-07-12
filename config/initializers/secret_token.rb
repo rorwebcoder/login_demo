d895a8255e871ddc87f3b9ab0c63a3dea8b195ce
Vadi <ror.webcoder@gmail.com>
2013-07-12 07:28:10 +0530

    Initial commit. Authlogic login, user registration func implemented

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginDemo::Application.config.secret_token = '253af9be1d90ae277c7bbf15ecec0c33057431de29f6a701cb86202e08348084210deff77c0316454536f1c642dccab56e5542dfeedd3d6c7d35996dd14b43d7'
